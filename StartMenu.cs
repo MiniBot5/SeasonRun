59f030ccf5b0942906ea5c2e5304413d04175f9d
Wout-Tahon <wout.tahon@gmail.com>
2025-09-27 18:13:01 +0200

    Start Menu

---
+++ StartMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject startMenuUI;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+        startMenuUI.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        startMenuUI.SetActive(false);
+    }
+}
